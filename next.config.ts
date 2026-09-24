be29a7e801b7007a88cfc5169148eeb02d00605d
pkraiwit2614-web <p.kraiwit2614@gmail.com>
2026-09-24 12:15:23 +0700

    Type browser module replacement handler

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ const nextConfig: NextConfig = {
   webpack(config, { isServer, webpack }) {
     if (!isServer) {
       config.plugins.push(
-        new webpack.NormalModuleReplacementPlugin(/^node:/, (resource) => {
+        new webpack.NormalModuleReplacementPlugin(/^node:/, (resource: { request: string }) => {
           resource.request = resource.request.replace(/^node:/, '')
         })
       )
