ded6b9aa319c6121ee6972ff0b377721759f21db
pkraiwit2614-web <p.kraiwit2614@gmail.com>
2026-09-24 12:14:19 +0700

    Fix browser bundling for PowerPoint exporter

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,23 @@ const nextConfig: NextConfig = {
         pathname: '/storage/v1/object/sign/**'
       }
     ]
+  },
+  webpack(config, { isServer, webpack }) {
+    if (!isServer) {
+      config.plugins.push(
+        new webpack.NormalModuleReplacementPlugin(/^node:/, (resource) => {
+          resource.request = resource.request.replace(/^node:/, '')
+        })
+      )
+      config.resolve.fallback = {
+        ...(config.resolve.fallback || {}),
+        fs: false,
+        https: false,
+        os: false,
+        path: false
+      }
+    }
+    return config
   }
 }
 
