0899500c4ca6e0a3649e67419699ba6f4b4b9b05
pkraiwit2614-web <p.kraiwit2614@gmail.com>
2026-09-26 14:41:56 +0700

    Use verified Condo photos and explicit status meanings in presentation

--- app/presentation/page.tsx
+++ app/presentation/page.tsx
@@ -1,9 +1,11 @@
 import ExecutivePresentationV37 from '@/components/ExecutivePresentationV37'
 import ExecutivePhotoLinks from '@/components/ExecutivePhotoLinks'
+import PresentationCondoEnhancer from '@/components/PresentationCondoEnhancer'
 
 export default function ExecutivePresentationPage(){
   return <>
     <ExecutivePresentationV37 />
+    <PresentationCondoEnhancer />
     <ExecutivePhotoLinks />
   </>
 }
